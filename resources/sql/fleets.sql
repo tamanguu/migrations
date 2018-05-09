7ed15a3f51a5f57cf8d3f92a5db5a14c6fe27461
Dr. Wolfram Schroers <Wolfram.Schroers@Field-theory.org>
2018-05-09 15:14:19 +0200

    Add basic getters

---
+++ resources/sql/fleets.sql
@@ -0,0 +1,19 @@
+-- region Public functions
+
+-- :name get-version :? :1
+-- :doc Return the schema version as a string
+SELECT version FROM FleetSchema;
+
+-- :name get-captains :? :*
+-- :doc Return all captains
+SELECT * FROM Captains;
+
+-- :name get-starships :? :*
+-- :doc Return all starships
+SELECT starshipID, creationDate, (SELECT Captains.name FROM Captains WHERE Starships.captainID = Captains.captainID) AS captain, name FROM Starships;
+
+-- :name get-fleets :? :*
+-- :doc Return all fleets
+SELECT *, ARRAY(SELECT starshipID FROM ShipsInFleet WHERE ShipsInFleet.fleetID = Fleets.fleetID) AS ships FROM Fleets;
+
+-- endregion
