4690b7eecd80f84747715806b5b26837717de957
Dr. Wolfram Schroers <Wolfram.Schroers@Field-theory.org>
2018-05-09 16:25:13 +0200

    Canonify reference to column

--- resources/dbmigrations/20180509123505-add-entity-object.down.sql
+++ resources/dbmigrations/20180509123505-add-entity-object.down.sql
@@ -5,9 +5,9 @@ DELETE FROM FleetSchema;
 INSERT INTO FleetSchema (version) VALUES ('1.0');
 
 -- Drop entity object and enumeration type
-ALTER TABLE Captains DROP COLUMN IF EXISTS object CASCADE;
-ALTER TABLE Starships DROP COLUMN IF EXISTS object CASCADE;
-ALTER TABLE Fleets DROP COLUMN IF EXISTS object CASCADE;
+ALTER TABLE Captains DROP COLUMN IF EXISTS "object" CASCADE;
+ALTER TABLE Starships DROP COLUMN IF EXISTS "object" CASCADE;
+ALTER TABLE Fleets DROP COLUMN IF EXISTS "object" CASCADE;
 DROP TYPE IF EXISTS ObjectType;
 
 -- endregion
